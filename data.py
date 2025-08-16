58e672c4e8958c52bf9be1e67d14a7494d57e6ec
wadibhasmeshreyash03 <shreyashwadibhasme407@gmail.com>
2025-08-16 12:53:31 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,10 @@
+data = {
+    "greetings": ["hello", "hi", "hey", "howdy", "hola", "good morning", "good afternoon", "good evening", "what's up", "yo"],
+    "responses": ["Hello!", "Hi there!", "Hey!", "Greetings!", "How can I help you?", "Nice to see you!", "Hi, how are you?", "Hey, what's going on?"],
+    "farewells": ["bye", "goodbye", "see you later", "take care", "farewell", "have a good day", "catch you later"],
+    "farewell_responses": ["Goodbye!", "See you later!", "Take care!", "Have a great day!", "Bye! Come back soon!", "Farewell!"],
+    "questions": ["how are you", "what's your name", "what can you do", "who created you", "what's the weather today"],
+    "question_responses": ["I'm just a bot, but I'm doing great!", "I'm your friendly chatbot!", "I can answer questions and chat with you.", "I was created by a developer.", "I don't know the weather, but you can check online!"],
+    "small_talk": ["tell me a joke", "what's new", "how's it going", "what are you up to"],
+    "small_talk_responses": ["Why don't scientists trust atoms? Because they make up everything!", "Not much, just here to chat with you!", "It's going well, thanks for asking!", "Just waiting to help you out!"],
+}
